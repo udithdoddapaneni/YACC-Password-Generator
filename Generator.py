577a0149d53901cb37eca18692fc9afee3cc0aee
Vishesh <112301037@smail.iitpkd.ac.in>
2024-06-27 02:47:03 +0530

    used the random module to generate the password done , some error handling left in case remaining_char are zero

--- Generator.py
+++ Generator.py
@@ -20,17 +20,22 @@ def generate_password(password_range : list , min_uppercase , min_lowercase, min
     
     # Create pools of characters create upper_chars , lower_chars , digit_chars , symbol_chars , remaining_chars
     
-    
-    
+    upper_chars="".join(random.choices(string.ascii_letters[26:],k=min_uppercase))    #used string slicing to get characters
+    lower_chars="".join(random.choices(string.ascii_letters[:26],k=min_lowercase))
+    digit_chars="".join(random.choices(string.digits,k=min_digits))
+    symbol_chars="".join(random.choices(string.punctuation,k=min_symbols))
+    total_min_required=len(upper_chars+lower_chars+symbol_chars+digit_chars)    
     # Ensure remaining characters are randomly chosen from all pools
     remaining_length = password_length - total_min_required
     all_chars = string.ascii_letters + string.digits + string.punctuation       
-    remaining_chars = random.choices(all_chars, k=remaining_length)
+    remaining_chars = "".join(random.choices(all_chars, k=remaining_length))
     
     # Combine all characters and shuffle them to ensure randomness
-    
-    
+    password=upper_chars+lower_chars+symbol_chars+digit_chars+remaining_chars
+
+    random.shuffle(list(password))    #converting to list to shuffle
+    password="".join(password)        #back to a string
     # Join the list to form the final password string
-    
+    return password
     
 # define the main function and print the final output
