c6f6d498a5b859396f56ebcd87b484cd1db00cc0
vaibhzGH <133134416+vaibhzGH@users.noreply.github.com>
2024-07-01 16:12:22 +0530

    Update RandomNumberGenerator.py

--- CustomRandom/RandomNumberGenerator.py
+++ CustomRandom/RandomNumberGenerator.py
@@ -1,10 +1,20 @@
+import time
 
+def Linear_Congruential_Generator(seed, a=1664525, c=1013904223, m=2**32):
+	while True:
+		seed = (a * seed + c) % m
+		yield seed
 
-def Randint(a, b):
-    """
-    Returns a random integer between a and b inclusive.
-
-    You have to make a pseudo-random number generator
-    """
-
-    return 0
+def RandInt(a, b):
+	try:
+		if a > b:
+			raise ValueError("Lower bound must be less than or equal to upper bound")
+		current_time = time.time()
+		seed = int(time.time() * 1000)	
+		generator = Linear_Congruential_Generator(seed)
+		range = b - a + 1
+		rand_no = next(generator) % range + a
+		return rand_no
+	except ValueError as v:
+		print(f"Error occurred : {v}")
+		return None
