d8d21175396f2d8b575053fc18485654d4f325cb
Abhirami009 <127651331+Abhirami009@users.noreply.github.com>
2024-07-16 00:00:50 +0530

    Update Random.py

--- CustomRandom/Random.py
+++ CustomRandom/Random.py
@@ -1,32 +1,9 @@
 from RandomNumberGenerator import Randint
 
 def RandomChoice(s):
-	try:
-		if not isinstance(s, list):
-			raise TypeError("only list is expected")
-		if not s:
-			raise ValueError("list should not be empty")
-		index = Randint(0, len(s) - 1)
-		return s[index]
-	except ValueError as v:
-		print(f"Error occurred : {v}")
-		return None
-	except TypeError as t:
-		print(f"Error occurred : {t}")
-		return None
+	
 
 
 
 def RandomShuffle(s: list):
-	try:
-		if not isinstance(s, list):
-			raise TypeError("only list is expected")
-		if len(s) <= 1:
-			return
-		n = len(s)
-		for i in range(n - 1, 0, -1):
-			j = Randint( 0, i)
-			s[i], s[j] = s[j], s[i]
-	except TypeError as t:
-		print(f"Error: {t}")
-		return None
+	
